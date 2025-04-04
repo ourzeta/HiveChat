75fa672b0f932b9cec52f2a543c7d0754dea767b
Wu Hao <wuhaoworld@163.com>
2025-04-04 17:42:36 +0800

    修复 mcp 选中状态切换后台后丢失问题

--- app/components/AppPrepare.tsx
+++ app/components/AppPrepare.tsx
@@ -2,9 +2,12 @@
 import React, { useEffect } from 'react'
 import useGlobalConfigStore from '@/app/store/globalConfig';
 import { fetchAppSettings } from '@/app/admin/system/actions';
+import useMcpServerStore from '@/app/store/mcp';
+import { getMcpServersAndAvailableTools } from '@/app/chat/actions/chat';
 
 const AppPrepare = () => {
   const { setChatNamingModel } = useGlobalConfigStore();
+  const { setHasUseMcp, setMcpServers, setAllTools } = useMcpServerStore();
   useEffect(() => {
     const initializeAppSettings = async () => {
       const result = await fetchAppSettings('chatNamingModel');
@@ -16,6 +19,31 @@ const AppPrepare = () => {
     }
     initializeAppSettings();
   }, [setChatNamingModel]);
+
+  useEffect(() => {
+    const initializeMcpInfo = async () => {
+      const { mcpServers, tools } = await getMcpServersAndAvailableTools();
+      if (mcpServers.length > 0) {
+        setHasUseMcp(true);
+        setMcpServers(mcpServers.map(server => ({
+          ...server,
+          description: server.description ?? undefined,
+        })));
+        setAllTools(tools.map(tool => ({
+          id: tool.name,
+          name: tool.name,
+          serverName: tool.serverName,
+          description: tool.description || undefined,
+          inputSchema: JSON.parse(tool.inputSchema),
+        })))
+      } else {
+        setHasUseMcp(false);
+        setMcpServers([]);
+        setAllTools([]);
+      }
+    }
+    initializeMcpInfo();
+  }, [setHasUseMcp, setMcpServers, setAllTools]);
   return (
     <></>
   )
